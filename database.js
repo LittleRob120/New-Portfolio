d69b27685c0a27f27858c8f177bd7f7df120b541
Gabriel Robert Sousa da Silva <113473643+LittleRob120@users.noreply.github.com>
2025-06-22 19:36:18 -0300

    Comment out MySQL database connection code

--- database.js
+++ database.js
@@ -1,18 +1,18 @@
-const mysql2 = require('mysql2');
+// const mysql2 = require('mysql2');
 
-// const db = mysql2.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'fatec',
-//     database: 'Portfolio'
-// });
+// // const db = mysql2.createConnection({
+// //     host: 'localhost',
+// //     user: 'root',
+// //     password: 'fatec',
+// //     database: 'Portfolio'
+// // });
 
-// db.connect(err => {
-//     if (err) {
-//         console.error('Erro ao conectar ao banco:', err.message);
-//     } else {
-//         console.log('Conectado ao banco de dados MySQL');
-//     }
-// });
+// // db.connect(err => {
+// //     if (err) {
+// //         console.error('Erro ao conectar ao banco:', err.message);
+// //     } else {
+// //         console.log('Conectado ao banco de dados MySQL');
+// //     }
+// // });
 
-module.exports = db;
+// module.exports = db;
